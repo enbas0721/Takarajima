67bf6a9f23b848a4b5d9274908afa3f4331ae233
Kento Maruyama <chicken.72177@gmail.com>
2020-06-30 22:30:35 +0900

    I added TakeItOut Script

---
+++ Assets/Myscripts/TakeItOut.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TakeItOut : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObj;
+
+    [SerializeField]
+    private float changeDis;
+
+    [SerializeField]
+    private GameObject changedObj;
+
+    [SerializeField]
+    private TextMeshProUGUI distanceUI;
+
+    private float colliderOffset;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        colliderOffset = GetComponent<CapsuleCollider>().radius + targetObj.GetComponent<CapsuleCollider>().radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(transform.position, targetObj.transform.position) - colliderOffset;
+
+        if(distanceUI != null)
+        {
+            distanceUI.text = distance.ToString("0.00m");
+        }
+        else
+        {
+            Debug.Log(distance.ToString("0.00m"));
+        }
+
+        if(distance > changeDis)
+        {
+            targetObj.SetActive(false);
+            changedObj.SetActive(true);
+        }
+    }
+}
